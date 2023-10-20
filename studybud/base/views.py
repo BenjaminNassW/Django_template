11284918204ec1a5939ea5b53a09aa3103a940a2
Benjamin Nass <benjanass@gmail.com>
2023-10-19 21:10:21 -0300

    for en html

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id':1,'name':"Let's learn python"},
+    {'id':2,'name':"Design with me"},
+    {'id':3,'name':"Developers"},
+]
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms':rooms})
 
 
 def room(request):
